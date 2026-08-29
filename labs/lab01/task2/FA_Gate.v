57c975ecc142a57e62b1b41362fed01fc520255d
kanchanjoshi-cell <kanchanjoshi45678@gmail.com>
2026-08-29 16:17:33 +0000

    lab01 task2

--- labs/lab01/task2/FA_Gate.v
+++ labs/lab01/task2/FA_Gate.v
@@ -20,11 +20,6 @@
 // FA_Gate.v
 
 // Gate-level model of a 1-bit full adder with rise/fall delays.
-
-// FA_Gate.v
-
-// Gate-level model of a 1-bit full adder with explicit rise/fall delays.
-
 module FA_Gate(
 
   input  a,
@@ -38,14 +33,10 @@ module FA_Gate(
 
   wire ps, pc1, pc2;
 
-  // Part (b): rise/fall delays
-  // rise delay = 2
-  // fall delay = 3
-
-  xor #(2,3) (ps,  a,   b);
-  and #(2,3) (pc1, a,   b);
-  xor #(2,3) (sum, cin, ps);
-  and #(2,3) (pc2, cin, ps);
-  or  #(2,3) (cout, pc1, pc2);
+  xor #(2) (ps,  a,   b);
+  and #(2) (pc1, a,   b);
+  xor #(2) (sum, cin, ps);
+  and #(2) (pc2, cin, ps);
+  or  #(2) (cout, pc1, pc2);
 
 endmodule
